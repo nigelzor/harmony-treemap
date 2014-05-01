ee040e6786d4a3553a5d553529217bcd05ee1d9b
Neil Gentleman <ngentleman@gmail.com>
2014-04-30 18:05:12 -0700

    add guava-testlib for additional test coverage

---
+++ src/test/java/org/apache/harmony/java/util/GuavaTreeMapTest.java
@@ -0,0 +1,35 @@
+package org.apache.harmony.java.util;
+
+import com.google.common.collect.testing.SortedMapTestSuiteBuilder;
+import com.google.common.collect.testing.TestStringSortedMapGenerator;
+import com.google.common.collect.testing.features.CollectionFeature;
+import com.google.common.collect.testing.features.CollectionSize;
+import com.google.common.collect.testing.features.MapFeature;
+import com.google.common.collect.testing.testers.MapEntrySetTester;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import java.util.Map;
+import java.util.SortedMap;
+
+public class GuavaTreeMapTest extends TestSuite {
+
+	private static class TreeMapGenerator extends TestStringSortedMapGenerator {
+		@Override
+		protected SortedMap<String, String> create(Map.Entry<String, String>[] entries) {
+			TreeMap<String, String> map = new TreeMap<String, String>();
+			for (Map.Entry<String, String> entry : entries) {
+				map.put(entry.getKey(), entry.getValue());
+			}
+			return map;
+		}
+	}
+
+	public static Test suite() {
+		return SortedMapTestSuiteBuilder.using(new TreeMapGenerator())
+				.named("TreeMap")
+				.withFeatures(CollectionSize.ANY, MapFeature.GENERAL_PURPOSE, MapFeature.ALLOWS_NULL_VALUES, CollectionFeature.SUPPORTS_ITERATOR_REMOVE)
+				.suppressing(MapEntrySetTester.getContainsEntryWithIncomparableKeyMethod())
+				.createTestSuite();
+	}
+}
